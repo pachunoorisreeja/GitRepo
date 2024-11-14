c90830e3960c0e6f235085bb9abe8b328e6c0f08
pachunoorisreeja <35169632+pachunoorisreeja@users.noreply.github.com>
2024-11-14 20:50:59 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome! hi");
+        System.out.printf("Hello and welcome! hi sai");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
